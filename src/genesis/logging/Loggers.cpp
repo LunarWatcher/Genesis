6deb2bfb20fd979b80e2e3b1e1e29b1f1112e134
Olivia <zoe.i2k1@gmail.com>
2021-10-07 00:40:03 +0200

    Start hooking up proper logging

---
+++ src/genesis/logging/Loggers.cpp
@@ -0,0 +1,30 @@
+#include "genesis/rendering/Renderer.hpp"
+
+#include "spdlog/common.h"
+#include "spdlog/logger.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
+
+#include "stc/Environment.hpp"
+#include "stc/FS.hpp"
+
+#include <memory>
+
+namespace genesis {
+
+static std::vector<spdlog::sink_ptr> sinks = {
+    std::make_shared<spdlog::sinks::stdout_color_sink_mt>(),
+#ifndef GENESIS_DEBUG
+    std::make_shared<spdlog::sinks::basic_file_sink_mt>(
+#ifdef _WIN32
+            (stc::getHome() / "AppData/Local/Temp/genesis.log").string()
+#else
+            "/tmp/genesis.log"
+#endif
+    ),
+#endif
+};
+
+auto Renderer::logger = std::make_shared<spdlog::logger>("rendererer", sinks.begin(), sinks.end());
+
+}
