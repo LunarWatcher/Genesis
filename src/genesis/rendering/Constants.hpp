ff306c0ad8c667d32a704dd7edb2216da926ea99
Olivia <zoe.i2k1@gmail.com>
2021-11-07 23:21:48 +0100

    Prematurely add cube vertices
    Not sure if they even work
    
    The plan for these, for the record, is to make the rendered world 3D-rendered, but still top-down. This is purely to make the map, well, not look like trash

--- src/genesis/rendering/Constants.hpp
+++ src/genesis/rendering/Constants.hpp
@@ -15,9 +15,26 @@ inline const std::vector<GLfloat> square = {
 
 inline const std::vector<GLint> squareIndices = {0, 1, 3, 3, 1, 2};
 
-constexpr int MAX_OVERWORLD_HEIGHT = 32;
-constexpr int MAX_UNDERGROUND_HEIGHT = 32;
-constexpr int FLAT_COMPENSATION_FACTOR = 12;
+inline const std::vector<GLfloat> cube = {
+    0.0, 0.0, 1.0,
+    1.0, 0.0, 1.0,
+    0.0, 1.0, 1.0,
+    1.0, 1.0, 1.0,
+    0.0, 0.0, 0.0,
+    1.0, 0.0, 0.0,
+    0.0, 1.0, 0.0,
+    1.0, 1.0, 0.0,
+};
+
+// TODO: create a cache of possible cube vertices based on which sides are free.
+inline const std::vector<GLint> fullCubeVertices = {
+    0, 1, 2, 3, 7, 1, 5, 4, 7, 6, 2, 4, 0, 1
+};
+
+// I'm not convinced this is the best way to go about this.
+//constexpr int MAX_OVERWORLD_HEIGHT = 32;
+//constexpr int MAX_UNDERGROUND_HEIGHT = 32;
+//constexpr int FLAT_COMPENSATION_FACTOR = 12;
 
 } // namespace Constants
 } // namespace genesis
